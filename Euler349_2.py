73822d418603cda40e542ee7296b9084ecbcaab8
AndrewMoss314 <77117249+AndrewMoss314@users.noreply.github.com>
2021-01-09 15:39:52 +0000

    Add files via upload

---
+++ Euler349_2.py
@@ -0,0 +1,57 @@
+def colourCurrentLocation(location, grid):
+    if grid == []:
+        return "W"
+    if str(location) in grid:
+        return "B"
+    else:
+        return "W"
+
+
+# print(colourCurrentLocation([0, 0], [[0, 0], [1, 0]]))
+# print(colourCurrentLocation([0, 1], [[0, 0], [1, 0]]))
+# print(colourCurrentLocation([0, 1], []))
+
+
+# print(currentGrid)
+# print(type(currentGrid))
+
+def run():
+    maxT = 1000000
+
+    currentLocation = [0, 0]
+    currentOrientation = 0  # 0=Up, 1=Right, 2=Down, 3 = right
+    currentGrid = []  # records black cells
+
+    for t in range(0, maxT):
+        currentLocationColour = colourCurrentLocation(
+            currentLocation, currentGrid)
+
+        if str(currentLocation) in currentGrid:
+            currentGrid.remove(str(currentLocation))
+        else:
+            currentGrid.append(str(currentLocation))
+
+        if currentLocationColour == "W":
+            currentOrientation = (currentOrientation + 1) % 4
+        else:  # on black
+            currentOrientation = (currentOrientation - 1) % 4
+
+        if currentOrientation == 0:
+            currentLocation[0] = currentLocation[0]
+            currentLocation[1] = currentLocation[1] + 1
+        elif currentOrientation == 1:
+            currentLocation[0] = currentLocation[0] + 1
+            currentLocation[1] = currentLocation[1]
+        elif currentOrientation == 2:
+            currentLocation[0] = currentLocation[0]
+            currentLocation[1] = currentLocation[1] - 1
+        elif currentOrientation == 3:
+            currentLocation[0] = currentLocation[0] - 1
+            currentLocation[1] = currentLocation[1]
+
+        # print(t, currentGrid, currentLocation,
+            #   currentOrientation, currentLocationColour)
+    print(len(currentGrid))
+
+
+run()
